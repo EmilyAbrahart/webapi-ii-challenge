7668042226ee0611e169e73934a696f92f1369b3
EmilyAbrahart <emilyabrahart@gmail.com>
2019-07-09 13:31:04 +0100

    get posts and get posts by id filled out and tested

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ server.use('/api/posts', postsRoutes);
 
 server.listen(3000, () => {
   console.log('listening on port 3000')
-})
+});
--- posts/postsRoutes.js
+++ posts/postsRoutes.js
@@ -1,10 +1,37 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const Post = require('../data/db');
 
-router.get('/', (req, res) => {})
+// Get all posts
+router.get('/', (req, res) => {
+  Post.find()
+  .then(data => {
+    res.status(200).json(data);
+  })
+  .catch(error => {
+    res
+    .status(500)
+    .json({error: "The posts information could not be retrieved."})
+  })
+})
 
+// Get post by ID
 router.get('/:id', (req, res) => {
   const id = req.params.id;
+  Post.findById(id)
+  .then(data => {
+    if (data) {
+      res.status(200).json(data);
+    } else {
+      res
+        .status(404)
+        .json({ message: 'The post with the specified ID does not exist.' });
+    }
+  })
+  .catch(error => {
+    res
+      .status(500)
+      .json({ error: 'The post could not be retrieved.' });
+  });
 });
 
 router.get('/:id/comments', (req, res) => {
